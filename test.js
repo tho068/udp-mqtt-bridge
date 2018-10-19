09c370960f4a7305603c9dd40e37efa35205794c
Pontus Aurdal <pontus.edvard@gmail.com>
2018-10-19 17:21:00 +0200

    Refactor. Fix bug where hash comparasion was case sensitive

--- test.js
+++ test.js
@@ -1,12 +1,20 @@
-var PORT = 8085;
-var HOST = '127.0.0.1';
+const dgram = require('dgram')
+const PORT = 8081
+const HOST = '127.0.0.1'
+const message = new Buffer(JSON.stringify({
+  thingName: '',
+  temp: 26,
+  hum: 33,
+  auth: ''
+}))
 
-var dgram = require('dgram');
-var message = new Buffer(JSON.stringify({thingName: '00001511', temp: 26, hum: 33, auth: 'ba0bdc0e32cf508420af18db374b9b30'}));
-
-var client = dgram.createSocket('udp4');
-client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
-    if (err) console.log(err)
-    console.log('UDP message sent to ' + HOST +':'+ PORT);
-    client.close();
-});
+var client = dgram.createSocket('udp4')
+client.send(message, 0, message.length, PORT, HOST, (err, bytes) => {
+  if (err) {
+    console.log(err)
+    return
+  }
+  
+  console.log(`UDP message sent to ${HOST}:${PORT}`)
+  client.close()
+})
--- utils.js
+++ utils.js
@@ -2,133 +2,148 @@ const mic = require('mic-sdk-js').default
 const fs = require('fs')
 const AdmZip = require('adm-zip')
 const awsIot = require('aws-iot-device-sdk')
-const crypto = require('crypto');
+const crypto = require('crypto')
 
-function fixPath(str){
-    if(str.substr(-1) === '/') {
-        return str.substr(0, str.length - 1);
-    }
-    return str;
+const fixPath = str => {
+  if (str.substr(-1) === '/')
+    return str.substr(0, str.length - 1)
+  return str
 }
 
 module.exports = {
-    /* Get new certificate from MIC */
-    getCertificate: (username, password, thingName, path) => {
-        return new Promise((Resolve, Reject) => {
-            const api = new mic
-            api.init ('startiot.mic.telenorconnexion.com')
-                .then( (manifest, credentials) => {
-                    api.login (username, password)
-                        .then (user => {
-                            const payload = {
-                                action: 'DOWNLOAD_CERTIFICATE',
-                                attributes: {
-                                    thingName
-                                }
-                            }
-                            api.invoke('ThingLambda', payload)
-                                .then (data => {
-                                    fs.writeFile(`${fixPath(path)}/${thingName}.zip`, Buffer.from(data, 'base64'), (err) => {
-                                        if (err) {
-                                            console.log(err)
-                                        }
-                            
-                                        /* Unzip the file */
-                                        let zip = new AdmZip(`${fixPath(path)}/${thingName}.zip`)
-                                        zip.extractAllTo(`${fixPath(path)}/`, true)
-                            
-                                        /* Delete tmp zip */
-                                        fs.unlink(`${fixPath(path)}/${thingName}.zip`, (err) => {
-                                            if (err) console.log(err);
-                                        })
-    
-                                        Resolve()
-                                    })
-                                })
-                                .catch(e => {
-                                    console.log(e)
-                                    Reject(e)
-                                })
-                            })
-                            .catch(e => {
-                                console.log(e)
-                                Reject(e)
-                            })
-                    })
-                    .catch(e => {
-                        console.log(e)
-                        Reject(e)
-                    })
-        })
-    },
 
-    /* Check if certificate exists in folder */
-    isValidCert: async (pathToDir, thingName) => {
-        return new Promise((Resolve, Reject) => {
-            const path = fixPath(pathToDir)
-            fs.access(`${path}/${thingName}`, fs.constants.F_OK, (err) => {
-                if(err) {
-                    Reject()
-                } else {
-                Resolve()
-                }
+  /* Check if certificate exists in specified
+   * folder. Throw exception if not found.
+   */
+  isValidCert: async (pathToDir, thingName) => {
+    return new Promise((resolve, reject) => {
+      const path = fixPath(pathToDir)
+      fs.access(`${path}/${thingName}`, fs.constants.F_OK, err => {
+        if(err) {
+          reject()
+        } else {
+          resolve()
+        }
+      })
+    })
+  },
+
+  /* Verify incoming md5 hash with stored certificate md5 hash.
+   */
+  verifyMessage: (hash, thingName, path) => {
+    return new Promise((resolve, reject) => {
+      try {
+        // Non-existing hash
+        if (hash === null || hash === '') {
+          resolve(false)
+          return
+        }
+
+        // Read stored certificate md5 hash
+        fs.readFile(`${fixPath(path)}/${thingName}/pubkey.pem`, (err, data) => {
+          const match = crypto.createHash('md5').update(data).digest('hex')
+          
+          // Verify that Thing hash and local hash are the same
+          if (String(hash).toLowerCase() === String(match).toLowerCase()) {
+            resolve(true)
+            return
+          }
+
+          // Not same
+          resolve(false)
+        })
+
+      // Unknown error occured
+      } catch(e) {
+        reject(e)
+      }
+    })
+  },
+
+  /* Send MQTT message to MIC.
+   */
+  sendMessage: (thingName, payload, path) => {
+    try {
+      const device = awsIot.device({
+        keyPath: `${fixPath(path)}/${thingName}/privkey.pem`,
+        certPath: `${fixPath(path)}/${thingName}/cert.pem`,
+        caPath: `${fixPath(path)}/ca.txt`,
+        clientId: thingName,
+        host: 'a3k7odshaiipe8.iot.eu-west-1.amazonaws.com'
+      })
+
+      device.on('connect', () => {
+        const topic = `$aws/things/${thingName}/shadow/update`
+        const message = {
+          state: {
+            reported: {
+              ...payload
+            }
+          }
+        }
+
+        // Publish and disconnect
+        device.publish(topic, JSON.stringify(message), (err) => {
+          if (err) {
+            console.log('Error during MQTT publish:', err.toString())
+          }
+          console.log('%s\x1b[32m%s\x1b[0m%s', '\t⮡ sucessfully relayed ', JSON.stringify(message), ' to MIC')
+          device.end()
+        })
+      })
+    } catch (e) {
+      console.log('Error during sendMessage:', e)
+    }
+  },
+
+  /* Get new certificate from MIC */
+  getCertificate: (username, password, thingName, path) => {
+    return new Promise((resolve, reject) => {
+      const api = new mic
+      api.init('startiot.mic.telenorconnexion.com')
+        .then((manifest, credentials) => {
+          api.login(username, password)
+            .then(user => {
+              const payload = {
+                action: 'DOWNLOAD_CERTIFICATE',
+                attributes: { thingName }
+              }
+
+              api.invoke('ThingLambda', payload)
+                .then(data => {
+                  fs.writeFile(`${fixPath(path)}/${thingName}.zip`, Buffer.from(data, 'base64'), err => {
+                    if (err) {
+                      console.log(err)
+                      return
+                    }
+
+                    // Unzip the file
+                    let zip = new AdmZip(`${fixPath(path)}/${thingName}.zip`)
+                    zip.extractAllTo(`${fixPath(path)}/`, true)
+
+                    /* Delete tmp zip */
+                    fs.unlink(`${fixPath(path)}/${thingName}.zip`, (err) => {
+                      if (err)
+                        console.log(err)
+                    })
+
+                    resolve()
+                  })
+                })
+                .catch(e => {
+                  console.log(e)
+                  reject(e)
+                })
+            })
+            .catch(e => {
+              console.log(e)
+              reject(e)
             })
         })
-    },
-
-    /* Send MQTT message to MIC */
-    sendMessage: (thingName, payload, path) => {
-        try {
-            var device = awsIot.device({
-                keyPath: `${fixPath(path)}/${thingName}/privkey.pem`,
-                certPath: `${fixPath(path)}/${thingName}/cert.pem`,
-                caPath: `${fixPath(path)}/ca.txt`,
-                clientId: thingName,
-                host: 'a3k7odshaiipe8.iot.eu-west-1.amazonaws.com'
-            });
-            
-            device.on('connect', function () {
-                let topic = `$aws/things/${thingName}/shadow/update`
-                let message = {
-                state: {
-                    reported: {
-                    ...payload
-                    }
-                }
-                }
-            
-                /* Publish and disconnect */
-                device.publish(topic, JSON.stringify(message), (err) => {
-                if (err) {
-                    console.log(err.toString())
-                }
-                device.end()
-                });
-            });
-        } catch(e) {
-            console.log(e)
-        }
-    },
-
-    /* Verify the device publishing a message to MIC */
-    verifyMessage: (hash, thingName, path) => {
-        return new Promise((Resolve, Reject) => {
-            try{
-                if(hash == null){
-                    Resolve(false)
-                    return
-                }
-                fs.readFile(`${fixPath(path)}/${thingName}/pubkey.pem`, function(err, data){
-                    let match = crypto.createHash('md5').update(data).digest("hex")
-                    if( hash === match ){
-                        Resolve(true)
-                        return
-                    }
-                    Resolve(false)
-                })
-            } catch(e){
-                Reject(e)
-            }
+        .catch(e => {
+          console.log(e)
+          reject(e)
         })
-    },
+    })
+  }
 }
